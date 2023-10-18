c7f303945941a5e9c120ab1074f191238009269c
DanielFGray <danielfgray@gmail.com>
2023-10-18 11:43:30 -0500

    update auth permissions for mods

--- app/lib/index.tsx
+++ app/lib/index.tsx
@@ -22,40 +22,19 @@ export function uniq<T>(input: Array<T>): Array<T> {
   return Array.from(new Set(input));
 }
 
-export function extractError(error: null): null;
-export function extractError(error: Error): Error;
-export function extractError(error: ApolloError): GraphQLError;
-export function extractError(error: GraphQLError): GraphQLError;
-export function extractError(
-  error: null | Error | ApolloError | GraphQLError,
-): null | Error | GraphQLError;
-export function extractError(
-  error: null | Error | ApolloError | GraphQLError,
-): null | Error | GraphQLError {
-  return (
-    (error &&
-      "graphQLErrors" in error &&
-      error.graphQLErrors &&
-      error.graphQLErrors.length &&
-      error.graphQLErrors[0]) ||
-    error
-  );
-}
-
-export function getExceptionFromError(error: null | Error | ApolloError | GraphQLError):
+export function getExceptionFromError(error: null | Error | GraphQLError):
   | (Error & {
       code?: string;
       fields?: string[];
       extensions?: { code?: string; fields?: string[] };
     })
   | null {
-  // @ts-ignore
-  const graphqlError: GraphQLError = extractError(error);
-  const exception = graphqlError && graphqlError.extensions && graphqlError.extensions.exception;
-  return (exception || graphqlError || error) as Error | null;
+  // @ts-expect-error  extensions is non-standard
+  const exception = error && error.extensions && error.extensions.exception;
+  return (exception || error) as Error | null;
 }
 
-export function getCodeFromError(error: null | Error | ApolloError | GraphQLError): null | string {
+export function getCodeFromError(error: null | Error | GraphQLError): null | string {
   const err = getExceptionFromError(error);
   return err?.extensions?.code ?? err?.code ?? null;
 }
@@ -74,13 +53,20 @@ export function forbidWhen(
   request: Request,
 ) {
   const forbidWhen = when(AuthRestrict);
+  if (!forbidWhen) throw new Error("unknown auth restriction");
   const forbidsLoggedOut = forbidWhen & AuthRestrict.LOGGED_OUT;
   const forbidsLoggedIn = forbidWhen & AuthRestrict.LOGGED_IN;
   const forbidsNotMod = forbidWhen & AuthRestrict.NOT_MOD;
   const forbidsNotAdmin = forbidWhen & AuthRestrict.NOT_ADMIN;
-  if (currentUser && (forbidsLoggedIn || (forbidsNotAdmin && currentUser.role !== "ADMIN"))) {
+  if (
+    currentUser &&
+    (forbidsLoggedIn ||
+      (forbidsNotMod && currentUser.role === "USER") ||
+      (forbidsNotAdmin && currentUser.role !== "ADMIN"))
+  ) {
     throw redirect("/");
-  } else if (currentUser == null && forbidsLoggedOut) {
+  }
+  if (currentUser == null && forbidsLoggedOut) {
     const location = new URL(request.url);
     throw redirect(`/signup?redirectTo=${encodeURIComponent(location.pathname)}`);
   }
